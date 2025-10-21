40cf9b3174c2ae1ddcfef5459e4f9e3f1ab471fe
your_username <malahat.m82@gmail.com>
2025-10-21 13:13:52 +0200

    correcting email validation.

--- backend/src/validation/userValidation.ts
+++ backend/src/validation/userValidation.ts
@@ -5,7 +5,7 @@ export const registerSchema = z.object({
     .string()
     .min(3, "Username must be at least 3 characters")
     .max(20, "Username must be at most 20 characters"),
-  email: z.string().email("Invalid email address"),
+  email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
   userscar: z
     .string()
