ac78a281d204d011d0edd56ea44d7b3a4cc69c3a
your_username <malahat.m82@gmail.com>
2025-10-21 12:37:47 +0200

    add DynamoDB configuration and connection setup

---
+++ backend/src/data/dynamo.ts
@@ -0,0 +1,25 @@
+// import av db, nycklar och region osv
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+
+
+const accessKeyId: string = process.env.AWS_ACCESS_KEY_ID || '';
+const secretAccessKey: string = process.env.AWS_SECRET_ACCESS_KEY || '';
+const tableName: string = process.env.TABLE_NAME || '';
+
+
+const client: DynamoDBClient = new DynamoDBClient({
+  region: process.env.AWS_REGION || "eu-north-1",
+  credentials: {
+	accessKeyId: accessKeyId!,
+	secretAccessKey: secretAccessKey!,
+  },
+});
+
+const db: DynamoDBDocumentClient = DynamoDBDocumentClient.from(client);
+
+
+export { db, tableName };
+
+
